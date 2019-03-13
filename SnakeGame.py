742631aed17d7b388eaeba8cf1c17773741a7351
Mustafadagkiranlar <43909833+Mustafadagkiranlar@users.noreply.github.com>
2019-03-13 19:53:16 +0400

    Add files via upload

--- SnakeGame.py
+++ SnakeGame.py
@@ -19,7 +19,7 @@ wn.tracer(0)
 head = turtle.Turtle()
 # animation speed of the turtle
 head.speed(0)
-head.shape("square")
+head.shape("circle")
 head.color("white")
 head.penup()
 head.goto(0, 0)
@@ -28,7 +28,7 @@ head.direction = "stop"
 # snake food
 food = turtle.Turtle()
 food.speed(0)
-food.shape("circle")
+food.shape("square")
 food.color("yellow")
 food.penup()
 food.goto(0, 100)
@@ -76,6 +76,20 @@ wn.onkeypress(go_right, "d")
 # Main game loop
 while True:
     wn.update()
+
+    # Check for a collision with the border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1) # Pause game for 1 Sec
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
     # check for a collision with the food
     if head.distance(food) < 20: # head of the snake is 20 by 20
         # move the food to a random place
@@ -83,11 +97,25 @@ while True:
         y = random.randint(-290, 290)
         food.goto(x, y)
 
-        # add a segment
-        new_segment = turtle.Turtle
+        # Add a segment
+        new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("circle")
+        new_segment.color("grey")
+        new_segment.penup() # Make sure to dont draw on the screen
+        segments.append(new_segment)
 
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1): # ref : https://www.youtube.com/watch?v=DxVPN1PIuLM&t=121s (6:00)
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
 
     move()
 
